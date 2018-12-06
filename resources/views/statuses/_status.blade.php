470f914b1fdf68cd73211057c0123069d189392c
chenjianeng <751855556@qq.com>
2018-12-06 12:57:53 +0000

    zancun

---
+++ resources/views/statuses/_status.blade.php
@@ -0,0 +1,13 @@
+<li>
+    <a href="{{ route('users.show', $user->id) }}">
+        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
+    </a>
+    <a href="{{ route('users.show', [$user->id]) }}">
+        {{ $user->name }}
+
+    </a>
+    <span class="right">{{ $status->created_at->diffForHumans() }}</span>
+    <div class="layui-input-block">
+        {{ $status->content }}
+    </div>
+</li>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.default')
 @section('title', $user->name)
 @section('content')
-    @include('shared._user_info', ['user'=>$user])
+    <div class="container">
+        @include('shared._user_info', ['user'=>$user])
+        @if(count($statuses) > 0)
+            <ol id="user_statuses">
+                @foreach($statuses as $status)
+                    @include('statuses._status')
+                @endforeach
+            </ol>
+            {!! $statuses->render() !!}
+        @endif
+    </div>
 @stop
